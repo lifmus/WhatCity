e63d376b0fb926565ee98a5f78fc0672a7baeb99
Andrew Lin <f50ajl@gmail.com>
2012-07-22 19:35:23 -0700

    change environment file to precompile bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Gmaps::Application.initialize!
+
+
+#bootstrap
+config.assets.precompile += %w( bootstrap.css )
